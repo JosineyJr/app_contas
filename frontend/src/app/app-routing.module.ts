91e321b8afa4a23cadc50b4c872f7d1e6446370e
Josiney Junior <josineyjunior14@gmail.com>
2021-02-17 20:17:30 -0300

    adicionando a rota de contas

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -19,6 +19,10 @@ const routes: Routes = [
     path: 'login',
     loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
   },
+  {
+    path: 'conta',
+    loadChildren: () => import('./pages/conta/conta.module').then( m => m.ContaPageModule)
+  },
 ];
 
 @NgModule({
