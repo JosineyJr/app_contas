5774228587f6daa63cb30c93a0eda49b297df3c0
Josiney Junior <josineyjunior14@gmail.com>
2021-02-18 20:33:16 -0300

    mecanica minha conta

--- frontend/src/app/pages/conta/conta.page.ts
+++ frontend/src/app/pages/conta/conta.page.ts
@@ -1,15 +1,39 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Title } from '@angular/platform-browser';
 @Component({
   selector: 'app-conta',
   templateUrl: './conta.page.html',
   styleUrls: ['./conta.page.scss'],
 })
 export class ContaPage implements OnInit {
+  currentMonth = 50;
+  lastMonth = 100;
+  private sizeCircle = 725;
+  usuario = {
+    userName: null,
+    email: null,
+    password: null,
+  };
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(protected titleService: Title) {
+    this.titleService.setTitle('Minha Conta');
   }
 
+  getPercentage() {
+    const percentage = (this.currentMonth * 100) / this.lastMonth / 100;
+    console.log(percentage);
+    return percentage;
+  }
+
+  setStroke() {
+    return this.sizeCircle + this.sizeCircle * this.getPercentage();
+  }
+
+  arredondar(value) {
+    return Math.floor(value);
+  }
+
+  ngOnInit() {
+    this.usuario = JSON.parse(localStorage.getItem('loginBD'));
+  }
 }
