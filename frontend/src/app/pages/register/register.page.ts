24b6e3aff05e4f565f97128666b9958dfc145c5a
Josiney Junior <josineyjunior14@gmail.com>
2021-02-17 22:03:15 -0300

    adicionando mecanica de mostrar senha

--- frontend/src/app/pages/register/register.page.ts
+++ frontend/src/app/pages/register/register.page.ts
@@ -6,10 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.page.scss'],
 })
 export class RegisterPage implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
+  ngOnInit() {}
+  showPassword(input, text) {
+    if (input.el.type === 'password') {
+      text.style.width = '80px';
+      text.style.left = '250px';
+      text.innerText = 'Esconder';
+      input.el.type = 'text';
+    } else {
+      text.style.width = '60px';
+      text.style.left = '270px';
+      text.innerText = 'Mostrar';
+      input.el.type = 'password';
+    }
   }
-
 }
