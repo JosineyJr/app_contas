1bdbd66b37b1877efcc4fce6fab60c771f6171e6
Josiney Junior <josineyjunior14@gmail.com>
2021-02-18 16:33:01 -0300

    mudando o titulo da pagina de login

--- frontend/src/app/pages/login/login.page.ts
+++ frontend/src/app/pages/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  constructor(protected titleService: Title) {
+    this.titleService.setTitle('Log in');
   }
 
+  ngOnInit() {}
 }
