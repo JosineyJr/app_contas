f445074985f63f38235853b33287de1c97687781
Josiney Junior <josineyjunior14@gmail.com>
2021-02-18 16:35:12 -0300

    mudando o titulo da pagina de tipoConta

--- frontend/src/app/pages/tipo-conta/tipo-conta.page.ts
+++ frontend/src/app/pages/tipo-conta/tipo-conta.page.ts
@@ -1,15 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Title } from '@angular/platform-browser';
 @Component({
   selector: 'app-tipo-conta',
   templateUrl: './tipo-conta.page.html',
   styleUrls: ['./tipo-conta.page.scss'],
 })
 export class TipoContaPage implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  constructor(protected titleService: Title) {
+    this.titleService.setTitle('Tipo Contas');
   }
 
+  ngOnInit() {}
 }
