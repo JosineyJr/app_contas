70cdb303d119acdef39758221fb64523b5fbfd97
Josiney Junior <josineyjunior14@gmail.com>
2021-02-19 19:37:19 -0300

    adicionando rota de cricao de contas

--- frontend/src/app/pages/conta/conta-routing.module.ts
+++ frontend/src/app/pages/conta/conta-routing.module.ts
@@ -2,12 +2,17 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { ContaPage } from './conta.page';
+import { NovaContaComponent } from './nova-conta/nova-conta.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: ContaPage
-  }
+    component: ContaPage,
+  },
+  {
+    path: 'register',
+    component: NovaContaComponent,
+  },
 ];
 
 @NgModule({
