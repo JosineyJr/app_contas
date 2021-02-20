026c4b13eeb95df17a2041c1fd7a6476a37cefab
Josiney Junior <josineyjunior14@gmail.com>
2021-02-19 21:35:16 -0300

    configurando arquivo de cadastro de novas contas

--- frontend/src/app/pages/conta/conta.module.ts
+++ frontend/src/app/pages/conta/conta.module.ts
@@ -7,14 +7,10 @@ import { IonicModule } from '@ionic/angular';
 import { ContaPageRoutingModule } from './conta-routing.module';
 
 import { ContaPage } from './conta.page';
+import { NovaContaComponent } from './nova-conta/nova-conta.component';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
-    ContaPageRoutingModule
-  ],
-  declarations: [ContaPage]
+  imports: [CommonModule, FormsModule, IonicModule, ContaPageRoutingModule],
+  declarations: [ContaPage, NovaContaComponent],
 })
 export class ContaPageModule {}
