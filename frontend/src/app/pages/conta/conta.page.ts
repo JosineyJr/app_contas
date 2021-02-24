1b9f4ce3c82959156155912ada21e258b211e16b
Daniel Gonçalves <danielopesgl05@gmail.com>
2021-02-24 11:42:51 -0300

    Bolinha das conta funcionando(falta detalhes)

--- frontend/src/app/pages/conta/conta.page.ts
+++ frontend/src/app/pages/conta/conta.page.ts
@@ -7,8 +7,8 @@ import {NavController} from '@ionic/angular';
   styleUrls: ['./conta.page.scss'],
 })
 export class ContaPage implements OnInit {
-  currentMonth = 50;
-  lastMonth = 100;
+  currentMonth = 0;
+  lastMonth = 0;
   private sizeCircle = 725;
   contas: any;
   contasUsuario: any = [];
@@ -17,6 +17,7 @@ export class ContaPage implements OnInit {
     email: null,
     password: null,
   };
+  meses: any = ["Jan", "Fev", "Mar", "Abr", "Mai", "Jun", "Jul","Ago","Set","Out","Nov","Dez"];
 
   constructor(protected titleService: Title, protected navController: NavController) {
     this.titleService.setTitle('Minha Conta');
@@ -24,7 +25,6 @@ export class ContaPage implements OnInit {
 
   getPercentage() {
     const percentage = (this.currentMonth * 100) / this.lastMonth / 100;
-    console.log(percentage);
     return percentage;
   }
 
@@ -37,10 +37,17 @@ export class ContaPage implements OnInit {
   }
 
   ngOnInit() {
+    this.contas =  JSON.parse(localStorage.getItem('contaBD'));
+
+    if(!this.contas){
+      this.contas = [];
+      localStorage.setItem('contaBD', JSON.stringify(this.contas));
+    }
     if(JSON.parse(localStorage.getItem('loginBD'))){
       this.usuario = JSON.parse(localStorage.getItem('loginBD'));
       this.contas = JSON.parse(localStorage.getItem('contaBD'));
       this.verificarContas();
+      this.obterContas(this.contasUsuario);
     }else{
       this.navController.navigateBack("/login");
     }
@@ -52,5 +59,37 @@ export class ContaPage implements OnInit {
         this.contasUsuario.push(this.contas[i]);
       }
     }
+  }
+
+  obterData(data: string): string{
+    let date = new Date(data);
+    let dataFormatada = ((date.getDate() + " " + this.meses[(date.getMonth())] + " " + date.getFullYear()));
+    return dataFormatada;
+  }
+
+  obterContas(contas: any){
+    let currentDate = new Date();
+    for(var i = 0; i < contas.length; i++){
+      let data = new Date(contas[i].dataVencimento);
+      if(data.getMonth() === currentDate.getMonth()){
+        this.currentMonth += contas[i].valor;
+        console.log(this.currentMonth);
+      }else if(currentDate.getMonth() - 1 < 0){
+        let dez = 11;
+        if(data.getMonth() === dez){
+          this.lastMonth += contas[i].valor;
+        }
+      }else if(currentDate.getMonth() - 1 === data.getMonth()){
+        this.lastMonth +=  contas[i].valor;
+      }
+    }
+  }
+
+  totalContas(){
+    let total = 0;
+    for(var i = 0; i < this.contasUsuario.length; i++){
+      total += this.contasUsuario[i].valor;
+    }
+    return total;
   }
 }
