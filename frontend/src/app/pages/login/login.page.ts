a4e94c3d707f0c0265ce8e81c092b2039d5d89e2
Josiney Junior <josineyjunior14@gmail.com>
2021-02-24 19:19:11 -0300

    adicionando mecanica da mostrar e esconder senha

--- frontend/src/app/pages/login/login.page.ts
+++ frontend/src/app/pages/login/login.page.ts
@@ -63,4 +63,19 @@ export class LoginPage implements OnInit {
     });
     toast.present();
   }
+
+  showPassword(input, text) {
+    if (input.el.type === 'password') {
+      text.style.width = '80px';
+      text.style.left = '250px';
+      text.innerText = 'Esconder';
+      input.el.type = 'text';
+    } else {
+      text.style.width = '60px';
+      text.style.left = '270px';
+      text.innerText = 'Mostrar';
+      input.el.type = 'password';
+    }
+    return true;
+  }
 }
