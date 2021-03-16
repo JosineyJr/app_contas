f9a2849c275b70aeea8724fc6bee327d3889213f
Josiney Junior <josineyjunior14@gmail.com>
2021-03-16 18:40:28 -0300

    configurando rota de alterarSenha

--- frontend/src/app/pages/usuario/usuario-routing.module.ts
+++ frontend/src/app/pages/usuario/usuario-routing.module.ts
@@ -2,12 +2,17 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { UsuarioPage } from './usuario.page';
+import { AlterarSenhaComponent } from './alterar-senha/alterar-senha.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: UsuarioPage
-  }
+    component: UsuarioPage,
+  },
+  {
+    path: 'alterarSenha',
+    component: AlterarSenhaComponent,
+  },
 ];
 
 @NgModule({
--- frontend/src/app/pages/usuario/usuario.module.ts
+++ frontend/src/app/pages/usuario/usuario.module.ts
@@ -7,14 +7,10 @@ import { IonicModule } from '@ionic/angular';
 import { UsuarioPageRoutingModule } from './usuario-routing.module';
 
 import { UsuarioPage } from './usuario.page';
+import { AlterarSenhaComponent } from './alterar-senha/alterar-senha.component';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
-    UsuarioPageRoutingModule
-  ],
-  declarations: [UsuarioPage]
+  imports: [CommonModule, FormsModule, IonicModule, UsuarioPageRoutingModule],
+  declarations: [UsuarioPage, AlterarSenhaComponent],
 })
 export class UsuarioPageModule {}
