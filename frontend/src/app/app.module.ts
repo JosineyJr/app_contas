8b909e163c716326de322cbf01aa9925bdbce7ad
Josiney Junior <josineyjunior14@gmail.com>
2021-03-16 19:27:41 -0300

    adicionando serviceUsuario

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent],
@@ -17,6 +18,7 @@ import { AppRoutingModule } from './app-routing.module';
     AppRoutingModule,
     FormsModule,
     ReactiveFormsModule,
+    HttpClientModule,
   ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
---
+++ frontend/src/app/service/usuario-service.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Usuario } from '../interfaces/Usuario.interface';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UsuarioServiceService {
+  private API = 'http://localhost:8080/public/usuarios/';
+
+  constructor(private http: HttpClient) {}
+
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
+  };
+
+  getUsuarios() {
+    return this.http.get(this.API).toPromise();
+  }
+
+  getUsuario(id: number) {
+    let urlAuxiliar = this.API + id;
+    return this.http.get(urlAuxiliar).toPromise();
+  }
+
+  postUsuario(usuario: Usuario) {
+    if (usuario.id == null) {
+      return this.http
+        .post(this.API, JSON.stringify(usuario), this.httpOptions)
+        .toPromise();
+    }
+    return this.http
+      .put(this.API, JSON.stringify(usuario), this.httpOptions)
+      .toPromise();
+  }
+
+  deleteUsuario(id: number) {
+    let urlAuxiliar = this.API + id;
+    return this.http.delete(urlAuxiliar).toPromise();
+  }
+}
