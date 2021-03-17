bd0e6317abcc501fc59a7bae0ca7fa0667760a48
Josiney Junior <josineyjunior14@gmail.com>
2021-03-17 01:02:43 -0300

    design da pagina

--- frontend/src/app/pages/usuario/alterar-senha/alterar-senha.component.ts
+++ frontend/src/app/pages/usuario/alterar-senha/alterar-senha.component.ts
@@ -6,9 +6,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./alterar-senha.component.scss'],
 })
 export class AlterarSenhaComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {}
 
+  showPassword(input, text) {
+    if (input.el.type === 'password') {
+      text.style.width = '80px';
+      text.style.left = '250px';
+      text.innerText = 'Esconder';
+      input.el.type = 'text';
+    } else {
+      text.style.width = '60px';
+      text.style.left = '270px';
+      text.innerText = 'Mostrar';
+      input.el.type = 'password';
+    }
+    return true;
+  }
 }
